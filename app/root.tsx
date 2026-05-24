8b9a21d7e72d572b0a18009e35588f1d4b8e69f1
jesshdzz <jesushernandezz.dev@gmail.com>
2026-05-23 20:41:39 -0600

    agregar Outlet en lugar de children

--- app/root.tsx
+++ app/root.tsx
@@ -35,7 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <AuthProvider>
-          {children}
+          <Outlet />
         </AuthProvider>
         <ScrollRestoration />
         <Scripts />
--- app/routes.ts
+++ app/routes.ts
@@ -7,13 +7,13 @@ export default [
     route("auth/register", "routes/auth.register.tsx"),
 
     layout("routes/app.tsx", [
-        route("app", "routes/app._index.tsx"),
-        route('app/training', 'routes/app.training.tsx'),
-        route("app/training/:id", "routes/app.training.$id.tsx"),
-        route("app/routines", "routes/app.routines.tsx"),
-        route("app/routines/:id", "routes/app.routines.$id.tsx"),
-        route("app/progress", "routes/app.progress.tsx"),
-        route("app/profile", "routes/app.profile.tsx"),
+        route("app",            "routes/app._index.tsx"),
+        route('app/training',   'routes/app.training.tsx'),
+        // route("app/training/:id", "routes/app.training.$id.tsx"),
+        route("app/routines",   "routes/app.routines.tsx"),
+        // route("app/routines/:id", "routes/app.routines.$id.tsx"),
+        route("app/progress",   "routes/app.progress.tsx"),
+        route("app/profile",    "routes/app.profile.tsx"),
     ]),
     
 ] satisfies RouteConfig;
